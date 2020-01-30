b1c20e2f55acc1e665ecbeb51ab2049c49300711
rabiei <rabiei@jetsgram.ir>
2020-01-30 12:10:55 +0330

    adding product model adding actions and reducers for products initializing product overview page

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// combineReducers so that we can create one root reducer
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  products: productReducer
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+   <Provider store={store}>
+   </Provider>
   );
 }
 
---
+++ screens/shop/ProductOverViewScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {FlatList} from 'react-native';
+
+const ProductOverviewScreen = props =>{
+    <FlatList />
+}
+
+export default ProductOverviewScreen;
