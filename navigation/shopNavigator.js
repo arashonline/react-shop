2ed47c60ec6c368526e779a15e28c11d0b5a5ff5
rabiei <rabiei@jetsgram.ir>
2020-02-04 00:45:16 +0330

    connectiong product to productDetailScreen

--- navigation/shopNavigator.js
+++ navigation/shopNavigator.js
@@ -2,11 +2,13 @@ import {createAppContainer} from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import {Platform} from 'react-native'
 
-import productOverviewScreen from '../screens/shop/ProductOverviewScreen'
+import productOverviewScreen from '../screens/shop/ProductOverviewScreen';
+import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import Colors from '../constants/Colors';
 
 const ProductsNavigator = createStackNavigator({
-    ProductsOverview: productOverviewScreen
+    ProductsOverview: productOverviewScreen,
+    ProductDetail : ProductDetailScreen,
 },{
     defaultNavigationOptions: {
         headerStyle:{
