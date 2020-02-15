5c4f10206aa3ac2ecd1308e097bbfe1c3b8105fb
rabiei <rabiei@jetsgram.ir>
2020-02-15 23:48:32 +0330

    header button cart button

--- screens/shop/ProductOverViewScreen.js
+++ screens/shop/ProductOverViewScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import {FlatList, Text} from 'react-native';
+import {FlatList, Text, Platform} from 'react-native';
 import {useSelector, useDispatch} from 'react-redux';
+
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+
+import HeaderButton from '../../components/UI/HeaderButton';
 
 
 const ProductOverviewScreen = props =>{
@@ -32,6 +37,12 @@ const ProductOverviewScreen = props =>{
     />)
 }
 ProductOverviewScreen.navigationOptions = {
-    headerTitle: 'All Products'
+    headerTitle: 'All Products',
+    headerRight:()=>{
+        return (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+            <Item title='Cart' iconName={Platform.OS === 'android'?'md-cart':'ios-cart'} 
+            onPress={()=>{}} />
+        </HeaderButtons>);
+    }
 } 
 export default ProductOverviewScreen;
