f25ac8aeb825fe88c9a024d79536f3338f1cb1cd
rabiei <rabiei@jetsgram.ir>
2020-03-07 04:53:28 +0330

    Edit product

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const EditProductScreen = props => {
+    return (
+        <View>
+        <Text>THE EDIT PRODUCT</Text>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default EditProductScreen;
