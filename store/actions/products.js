63badb47d1b0ada7896c271fd0a798b94e8c467a
rabiei <rabiei@jetsgram.ir>
2020-03-08 08:11:31 +0330

    fetching data on server

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,20 +1,42 @@
-export const DELETE_PRODUCT = 'DELETE_PRODUCT';
-export const CREATE_PRODUCT = 'CREATE_PRODUCT';
-export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
 
+const createUrl= 'http://192.168.1.107:7009//api/product/store';
+
 export const createProduct = (title, description, imageUrl, price) => {
-  return {
-    type: CREATE_PRODUCT,
-    productData: {
-      title,
-      description,
-      imageUrl,
-      price
-    }
+  return async dispatch => {
+    // any async code you want!
+    const response = await fetch(createUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        imageUrl,
+        price
+      })
+    });
+
+    const resData = await response.json();
+      console.log(resData);
+
+    dispatch({
+      type: CREATE_PRODUCT,
+      productData: {
+        id: resData.id,
+        title,
+        description,
+        imageUrl,
+        price
+      }
+    });
   };
 };
 
@@ -25,7 +47,7 @@ export const updateProduct = (id, title, description, imageUrl) => {
     productData: {
       title,
       description,
-      imageUrl,
+      imageUrl
     }
   };
 };
