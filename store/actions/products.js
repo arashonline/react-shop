45d3f7c65a052692682efba845e1625d45ef4ff0
rabiei <rabiei@jetsgram.ir>
2020-03-09 03:22:26 +0330

    loading data from server

--- store/actions/products.js
+++ store/actions/products.js
@@ -1,20 +1,48 @@
+import Product from '../../models/product';
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const SET_PRODUCTS = "SET_PRODUCTS";
+
+const createProductUrl = "http://192.168.1.107:7009//api/product/add";
+const getProductsUrl = "http://192.168.1.107:7009//api/products";
+
+export const fetchProducts = () => {
+  return async dispatch => {
+    // any async code you want!
+    const response = await fetch(getProductsUrl);
+
+    const resData = await response.json();
+    const loadedProducts = [];
+
+    for(const key in resData){
+      loadedProducts.push( new Product(
+        resData[key].id,
+        resData[key].user_id,
+        resData[key].title,
+        resData[key].imageUrl,
+        resData[key].description,
+        resData[key].price
+      ))
+    }
+
+    console.log(loadedProducts)
+
+    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+  };
+};
 
 export const deleteProduct = productId => {
   return { type: DELETE_PRODUCT, pid: productId };
 };
 
-const createUrl= 'http://192.168.1.107:7009//api/product/store';
-
 export const createProduct = (title, description, imageUrl, price) => {
   return async dispatch => {
     // any async code you want!
-    const response = await fetch(createUrl, {
-      method: 'POST',
+    const response = await fetch(createProductUrl, {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         title,
@@ -25,7 +53,7 @@ export const createProduct = (title, description, imageUrl, price) => {
     });
 
     const resData = await response.json();
-      console.log(resData);
+    console.log(resData);
 
     dispatch({
       type: CREATE_PRODUCT,
