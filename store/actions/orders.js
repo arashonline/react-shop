8d7d8c5e15910e1a333d7e40a52f2f751fa1a0bb
rabiei <rabiei@jetsgram.ir>
2020-03-09 07:04:07 +0330

    adding order

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,5 +1,38 @@
-export const  ADD_ORDER = 'ADD_ORDER';
+export const ADD_ORDER = 'ADD_ORDER';
+
+const createOrderUrl = "http://192.168.1.107:7009/api/order/add?XDEBUG_SESSION_START=13069";
 
 export const addOrder = (cartItems, totalAmount) => {
-    return {type: ADD_ORDER, orderData:{items:cartItems, amount: totalAmount}}
+    return async  dispatch => {
+        const date = new Date();
+        const response = await fetch(createOrderUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                cartItems,
+                totalAmount,
+                date: date.toISOString()
+            })
+        });
+
+
+        if (!response.ok) {
+            throw new Error('Something went wrong');
+        }
+
+        const resData = await response.json();
+        dispatch(
+            {
+                type: ADD_ORDER,
+                orderData: {
+                    id: resData.id,
+                    items: cartItems,
+                    amount: totalAmount,
+                    date: date
+                }
+            }
+        )
+    }
 }
