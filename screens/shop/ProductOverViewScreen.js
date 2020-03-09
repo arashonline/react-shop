806495578af406ee332dc2ac0988f1944d9f12bd
rabiei <rabiei@jetsgram.ir>
2020-03-09 04:42:59 +0330

    reload product by changing page

--- screens/shop/ProductOverViewScreen.js
+++ screens/shop/ProductOverViewScreen.js
@@ -31,6 +31,13 @@ const ProductOverviewScreen = props => {
   },[dispatch, setIsLoading, setError])
 
   useEffect(() => {
+    const willFocusSub = props.navigation.addListener('willFocus', loadProducts );
+    return ()=>{
+      willFocusSub.remove()
+    }
+  }, [loadProducts]);
+
+  useEffect(() => {
     loadProducts();
   }, [dispatch,loadProducts]);
 
